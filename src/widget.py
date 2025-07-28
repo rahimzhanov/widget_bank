98f8fae2d1082ce1bba81d70f12de1d3996f41cc
Rakhimzhanov Aman <amanrahim2@gmail.com>
2025-07-29 00:22:01 +0600

    complete def

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,5 @@
-from .masks import get_mask_card_number
+from masks import get_mask_card_number
+from datetime import datetime
 
 
 def mask_account_card(account_card: str) -> str:
@@ -15,4 +16,18 @@ def mask_account_card(account_card: str) -> str:
     return masked
 
 
-# print(mask_account_card('Visa Platinum 7000792289606361'))
+def get_date(date_str: str) -> str:
+    """
+    Преобразует дату из формата 'ГГГГ-ММ-ДДTЧЧ:ММ:СС.микросекунды' в 'ДД.ММ.ГГГГ'
+
+    Args:
+        date_str: Строка с датой в ISO формате
+
+    Returns:
+        Строка с датой в формате 'ДД.ММ.ГГГГ'
+    """
+    dt = datetime.fromisoformat(date_str)
+    return dt.strftime("%d.%m.%Y")
+
+#print(mask_account_card('Visa Platinum 7000792289606361'))
+#print(get_date('2024-03-11T02:26:18.671407'))
