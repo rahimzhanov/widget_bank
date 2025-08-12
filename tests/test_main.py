baf4a81a7112ff2a2044263a2a83968c2d45eada
Rakhimzhanov Aman <amanrahim2@gmail.com>
2025-08-13 00:47:06 +0600

    add pytest

---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+from src.masks import get_mask_card_number
+from src.masks import get_mask_account
+from src.widget import mask_account_card
+from src.widget import get_date
+from src.processing import filter_by_state
+from src.processing import sort_by_date
+
+def test_get_mask_card_number():
+    assert get_mask_card_number()
